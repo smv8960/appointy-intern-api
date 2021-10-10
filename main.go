503b8d2feee4bc459d50d517b50faa1fa44b3169
Salman Mahboob <65416598+smv8960@users.noreply.github.com>
2021-10-10 07:37:15 +0530

    Update main.go

--- main.go
+++ main.go
@@ -6,10 +6,14 @@ import (
 	"fmt"
 	"net/http"
 	"time"
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
 
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	
 )
 
 var client *mongo.Client
@@ -25,28 +29,19 @@ func CreateUserEndpoint(response http.ResponseWriter, request *http.Request) {}
 func GetUsersEndpoint(response http.ResponseWriter, request *http.Request)   {}
 func GetUserEndpoint(response http.ResponseWriter, request *http.Request)    {}
 
-func main() {
-	fmt.Println("Starting the application...")
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-	client, _ = mongo.Connect(ctx, clientOptions)
-	router := mux.NewRouter()
-	router.HandleFunc("/user", CreateUserEndpoint).Methods("POST")
-	router.HandleFunc("/users", GetUsersEndpoint).Methods("GET")
-	router.HandleFunc("/user/{id}", GetUserEndpoint).Methods("GET")
-	http.ListenAndServe(":12345", router)
-}
-
+//to Create an User
 func CreateUserEndpoint(response http.ResponseWriter, request *http.Request) {
 	response.Header().Set("content-type", "application/json")
 	var user User
 	_ = json.NewDecoder(request.Body).Decode(&user)
+	//encrypting the password before uploding to databse
+	User.Password = HashPassword(User.Password)
 	collection := client.Database("instagram-backend").Collection("users")
 	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 	result, _ := collection.InsertOne(ctx, user)
 	json.NewEncoder(response).Encode(result)
 }
-
+//to Get a user using id
 func GetUserEndpoint(response http.ResponseWriter, request *http.Request) {
 	response.Header().Set("content-type", "application/json")
 	params := mux.Vars(request)
@@ -62,7 +57,7 @@ func GetUserEndpoint(response http.ResponseWriter, request *http.Request) {
 	}
 	json.NewEncoder(response).Encode(user)
 }
-func GetPeopleEndpoint(response http.ResponseWriter, request *http.Request) {
+func GetUsersEndpoint(response http.ResponseWriter, request *http.Request) {
 	response.Header().Set("content-type", "application/json")
 	var users []User
 	collection := client.Database("instagram-backend").Collection("users")
@@ -85,4 +80,24 @@ func GetPeopleEndpoint(response http.ResponseWriter, request *http.Request) {
 		return
 	}
 	json.NewEncoder(response).Encode(users)
+}
+//to hash encrypt the password
+func HashPassword(password string) string {
+    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+    if err != nil {
+        log.Panic(err)
+    }
+
+    return string(bytes)
+}
+func main() {
+	fmt.Println("Starting the application...")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, _ = mongo.Connect(ctx, clientOptions)
+	router := mux.NewRouter()
+	router.HandleFunc("/user", CreateUserEndpoint).Methods("POST")
+	router.HandleFunc("/users", GetUsersEndpoint).Methods("GET")
+	router.HandleFunc("/user/{id}", GetUserEndpoint).Methods("GET")
+	http.ListenAndServe(":12345", router)
 }
